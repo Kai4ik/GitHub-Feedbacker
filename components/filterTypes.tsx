4e75c060d15b0213aa7a1c1bded6e48ad6eab974
Kairat <orozobekov.kai@gmail.com>
2022-03-09 05:32:12 -0500

    GitHub Feedbacker app basic setup - Chakra Ui installed, TS configured

---
+++ components/filterTypes.tsx
@@ -0,0 +1,40 @@
+import { Button, Text, Flex } from "@chakra-ui/react";
+
+const FilterTypes = () => {
+  const types: Array<string> = [
+    "all",
+    "feature",
+    "bug (issue)",
+    "enhancement",
+    "new app idea",
+    "UI/UX",
+  ];
+
+  return (
+    <Flex
+      p="1rem"
+      wrap="wrap"
+      gridGap="10px 6px"
+      bg="#fff"
+      borderRadius="12px"
+      color="#3182CE"
+    >
+      {types.map((type, index) => (
+        <Button
+          size="xs"
+          key={index}
+          _hover={{
+            background: "#3182CE",
+            color: "#fff",
+          }}
+        >
+          <Text fontSize="0.6rem" textTransform="capitalize">
+            {type}
+          </Text>
+        </Button>
+      ))}
+    </Flex>
+  );
+};
+
+export default FilterTypes;
